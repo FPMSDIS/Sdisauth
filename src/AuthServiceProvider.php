ba9b94d1ea59a01d809c3443c255949eee23dc46
FPMSDIS <fpmsdis@gmail.com>
2025-02-10 18:14:17 +0000

    add route in service provider

--- src/AuthServiceProvider.php
+++ src/AuthServiceProvider.php
@@ -19,9 +19,40 @@ class AuthServiceProvider extends ServiceProvider
 
     public function boot()
     {   
-        //
+        $this->registerRoutes();
+        
+        // Log::info("Boot method called in AuthServiceProvider.");
+        // if (class_exists(\Spatie\Permission\PermissionServiceProvider::class)){
+        //     Log::info("avant route....");
+        //     $this->loadRoutesFrom(__DIR__.'/routes/userRolePermission.php');
+        //     Log::info("avant view");
+        //     $this->loadViewsFrom(__DIR__.'/resources/views', 'sdisauth');
+        //     Log::info("avant view pub");
+        //     $this->publishes([
+        //         __DIR__.'/resources/views' => resource_path('views/vendor/sdisauth'),
+        //     ], 'sdisauth');
+        //     Log::info("avant seeders");
+        //     $this->publishes([
+        //         __DIR__.'/database/seeders' => database_path('seeders'),
+        //     ], 'sdisauth');
+        //     Log::info("avant config");
+        //     $this->publishes([
+        //         __DIR__.'/config/sdisauth.php' => config_path('sdisauth.php'),
+        //     ], 'config');
+        //     Log::info("avant assets");
+        //     $this->publishes([
+        //         __DIR__.'/public' => public_path('vendor/sdisauth'),
+        //     ], 'sdisauth-assets');
+        // } else {
+        //     Log::warning("Spatie Permission Service Provider is not available.");
+        // }
         
     }
 
-    
+    protected function registerRoutes()
+    {
+        if (!$this->app->routesAreCached()) {
+            require __DIR__.'/routes/userRolePermission.php';
+        }
+    }
 }
